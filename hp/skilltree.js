6205dffabf4fc620bdc2402fdfd30a8727510cea
Kayoo <jeri.dsql@gmail.com>
2022-01-21 16:42:10 +0100

    Update skilltree.js

--- hp/skilltree.js
+++ hp/skilltree.js
@@ -3,7 +3,7 @@ var job1 = 0;
 var job2 = 0;
 var job3 = 0;
 
-console.log("-- share 5 --");
+console.log("-- share 1 --");
 
 /*
  * [Id, currentLevel, maxLevel]
@@ -123,15 +123,15 @@ function decNumber(skill) {
 }
 
 function shareLink() {
-	q_path = if (HolyLight[1] != 0) { "&HolyLight=" + HolyLight[1] } \
-		+ if (DemonBane[1] != 0) { "&DemonBane=" + DemonBane[1] } \
-		+ if (Heal[1] != 0) { "&Heal=" + Heal[1] } \
-		+ if (Teleport[1] != 0) { "&Teleport=" + Teleport[1] } \ 
-		+ if (Blessing[1] != 0) { "&Blessing=" + Blessing[1] } \
-		+ if (SignumCrucis[1] != 0) { "&SignumCrucis=" + SignumCrucis[1] } \
-		+ if (Angelus[1] != 0) { "&Angelus=" + Angelus[1] } \
-		+ if (IncreaseAgility[1] != 0) { "&IncreaseAgility=" + IncreaseAgility[1] } \
-		+ if (KyrieEleison[1] != 0) { "&KyrieEleison=" + KyrieEleison[1]} \
+	q_path = if (HolyLight[1] !== 0) { "&HolyLight=" + HolyLight[1] } \
+		+ if (DemonBane[1] !== 0) { "&DemonBane=" + DemonBane[1] } \
+		+ if (Heal[1] !== 0) { "&Heal=" + Heal[1] } \
+		+ if (Teleport[1] !== 0) { "&Teleport=" + Teleport[1] } \ 
+		+ if (Blessing[1] !== 0) { "&Blessing=" + Blessing[1] } \
+		+ if (SignumCrucis[1] !== 0) { "&SignumCrucis=" + SignumCrucis[1] } \
+		+ if (Angelus[1] !== 0) { "&Angelus=" + Angelus[1] } \
+		+ if (IncreaseAgility[1] !== 0) { "&IncreaseAgility=" + IncreaseAgility[1] } \
+		+ if (KyrieEleison[1] !== 0) { "&KyrieEleison=" + KyrieEleison[1]} \
 
 	console.log(q_path);
 	navigator.clipboard.writeText(q_path);
