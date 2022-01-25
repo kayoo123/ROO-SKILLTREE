34601632417b318dae4a5a7ad921f0f4cdf34743
Kayoo <jeri.dsql@gmail.com>
2022-01-25 22:11:24 +0100

    Update skilltree.js

--- hp/skilltree.js
+++ hp/skilltree.js
@@ -179,29 +179,13 @@ if(urlParams.get('HolyWard')) { HolyWard[1] = parseFloat(urlParams.get('HolyWard
 jobLevel();
 showSkill();
 
-//MODAL
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+/*
+ * Get Modal
+ */
+var modal = document.getElementById("myModal"); // Get the modal
+var btn = document.getElementById("myBtn"); // Get the button that opens the modal
+var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
+btn.onclick = function() { modal.style.display = "block"; } // When the user clicks the button, open the modal 
+span.onclick = function() { modal.style.display = "none"; } // When the user clicks on <span> (x), close the modal
+window.onclick = function(event) { if (event.target == modal) { modal.style.display = "none"; } } // When the user clicks anywhere outside of the modal, close it
