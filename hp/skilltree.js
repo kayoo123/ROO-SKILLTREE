17238b340f5b667e5f55a1a4c5533ad235957cff
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-01-27 16:08:10 +0100

    test

--- hp/skilltree.js
+++ hp/skilltree.js
@@ -285,7 +285,7 @@ function showModal(skill) {
 	//var btn = document.getElementById("myBtn"); // Get the button that opens the modal
 	var span = document.getElementsByClassName("close")[0]; // Get the <span> element that closes the modal
 	//btn.onclick = function() { modal.style.display = "block"; } // When the user clicks the button, open the modal 
-	document.getElementById(skill).style.display = "block";
+	document.getElementById("m1").style.display = "block";
 	span.onclick = function() { modal.style.display = "none"; } // When the user clicks on <span> (x), close the modal
 	window.onclick = function(event) { if (event.target == modal) { modal.style.display = "none"; } } // When the user clicks anywhere outside of the modal, close it	
 }
