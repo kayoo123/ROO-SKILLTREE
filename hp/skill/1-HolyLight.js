d185e6b9484962e02140e034469eefb37ab764f8
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-01-27 17:24:12 +0100

    test level

---
+++ hp/skill/1-HolyLight.js
@@ -0,0 +1,25 @@
+if(HolyLight[1] == 1) {
+    document.write(`
+    <p>lvl1</p>
+    `);
+}
+if(HolyLight[1] == 2) {
+    document.write(`
+    <p>lvl2</p>
+    `);
+}
+if(HolyLight[1] == 3) {
+    document.write(`
+    <p>lvl3</p>
+    `);
+}
+if(HolyLight[1] == 4) {
+    document.write(`
+    <p>lvl4</p>
+    `);
+}
+if(HolyLight[1] == 5) {
+    document.write(`
+    <p>lvl5</p>
+    `);
+}
