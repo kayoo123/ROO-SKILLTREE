fb44406114c9625a21f4cc0fd00e1dac02e177fd
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-01 16:26:28 +0100

    test

--- hp/skill/2-DemonBane.js
+++ hp/skill/2-DemonBane.js
@@ -1,9 +1,14 @@
 document.write(`
 
-    <h1>Add your HTML code here</h1>
-
-     <p>Notice, you do not have to escape LF's with a '\',
-        like demonstrated in the above code listing.
-    </p>
-
+    <img src="img/2.gif" alt="DemonBane" style="width:50px;height:50px;display:inline;">
+    <h1>Demon Bane - lv.1</h1>
+    <fieldset><legend>[Passive]</legend>	
+        <p> Gains [2/...] Base Physical Attack and [2/...] Base
+            Physical Defense. While attacking Undead 
+            or Demon monsters, gains [5/...] extra Base 
+            Physical Attack; when attacked by Undead
+            or Demon monsters, gains [5/...] extra Base
+            Physical Defense.
+        </p>
+    </fieldset>
 `);
