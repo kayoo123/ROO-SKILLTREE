91bfca2cee42b2079ae02e1d73258dd1ef0fc637
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-01 16:33:27 +0100

    test

--- hp/skill/2-DemonBane.js
+++ hp/skill/2-DemonBane.js
@@ -1,14 +1,12 @@
 document.write(`
 
-    <img src="img/2.gif" alt="DemonBane" style="width:50px;height:50px;display:inline;">
-    <h1>Demon Bane - lv.1</h1>
+    <img src="img/2.gif" alt="DemonBane" style="width:50px;height:50px;display:inline;"><h1>Demon Bane - lv.1</h1>
     <fieldset><legend>[Passive]</legend>	
-        <p> Gains [2/...] Base Physical Attack and [2/...] Base
-            Physical Defense. While attacking Undead 
-            or Demon monsters, gains [5/...] extra Base 
-            Physical Attack; when attacked by Undead
-            or Demon monsters, gains [5/...] extra Base
-            Physical Defense.
+        <p> Gains <b>[2/...]</b> Base Physical Attack and <b>[2/...]</b> Base Physical Defense. <br />
+            While attacking Undead or Demon monsters, gains <b>[5/...]</b> extra Base Physical Attack. <br />
+            When attacked by Undead or Demon monsters, gains <b>[5/...]</b> extra Base Physical Defense. <br />
         </p>
     </fieldset>
+    <hr>
+    <p><small>Attributes additionnaly gained only apply when you're battling Undead or Demon monsters</small></p>
 `);
