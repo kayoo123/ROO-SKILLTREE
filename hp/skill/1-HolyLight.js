3de705c1477e270893f1eb82aac98081898bc68b
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-01 16:49:47 +0100

    update

--- hp/skill/1-HolyLight.js
+++ hp/skill/1-HolyLight.js
@@ -5,18 +5,17 @@
 //console.log(lvl)
 
 document.write(`
-
-<img src="img/1.gif" alt="HolyLight" style="width:50px;height:50px;display:inline;"><h1>HolyLight</h1>
+    <h1><img src="img/1.gif" alt="HolyLight" style="width:50px;height:50px;display:inline;">HolyLight</h1>
     <fieldset><legend>[Magic Spell][Holy]</legend>
         <p> Prays to the Holy Light, attacking an enemy <br />
-            target and dealing <b>190/200/300%</br> + <b>60/70/80/90</b> Holy Magic <br />
+            target and dealing <b>190/200/300%</b> + <b>60/70/80/90</b> Holy Magic <br />
             damage to them.
         </p>	
         <hr>
-        <p> - Cast Time: (0,20s + XXs)
-            - Cooldown: Neutral
-            - SP Cost: 11
-            - Global Recovery: 0.5s
-        </p>
+        <p><small> - Cast Time: (0,20s + XXs)<br />
+            - Cooldown: Neutral<br />
+            - SP Cost: 11<br />
+            - Global Recovery: 0.5s<br />
+        </small></p>
     </fieldset>
 `);
--- hp/skill/2-DemonBane.js
+++ hp/skill/2-DemonBane.js
@@ -1,11 +1,11 @@
 document.write(`
-
-    <img src="img/2.gif" alt="DemonBane" style="width:50px;height:50px;display:inline;"><h1>Demon Bane - lv.1</h1>
+    <h1><img src="img/2.gif" alt="DemonBane" style="width:50px;height:50px;display:inline;">Demon Bane</h1>
     <fieldset><legend>[Passive]</legend>	
         <p> Gains <b>2/...</b> Base Physical Attack and <b>2/...</b> Base Physical Defense. <br />
             While attacking Undead or Demon monsters, gains <b>5/...</b> extra Base Physical Attack. <br />
             When attacked by Undead or Demon monsters, gains <b>5/...</b> extra Base Physical Defense.
         </p>
+        <hr>
+        <p><small>Attributes additionnaly gained only apply when you're battling Undead or Demon monsters</small></p>
     </fieldset>
-    <p><small>Attributes additionnaly gained only apply when you're battling Undead or Demon monsters</small></p>
 `);
