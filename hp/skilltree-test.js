cea77b4c5444baf587aa53b0b518173040ae32a2
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-01 17:29:32 +0100

    test

--- hp/skilltree-test.js
+++ hp/skilltree-test.js
@@ -281,8 +281,8 @@ jobLevel();
  */
 function showModal(modalId) {
 	var modal = document.getElementById(modalId); 
-	var span = document.getElementsByClassName("close")[0];
+	//var span = document.getElementsByClassName("close")[0];
 	modal.style.display = "block"; // Show modal
-	span.onclick = function() { modal.style.display = "none"; } // When the user clicks on <span> (x), close the modal
+	//span.onclick = function() { modal.style.display = "none"; } // When the user clicks on <span> (x), close the modal
 	window.onclick = function(event) { if (event.target == modal) { modal.style.display = "none"; } } // When the user clicks anywhere outside of the modal, close it	
 }
