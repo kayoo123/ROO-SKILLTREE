50127b707969c576131071b31f9fba1fcc7999b5
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 15:20:21 +0100

    add

---
+++ hp/skill/28-AdvancedMaceMastery.js
@@ -0,0 +1,11 @@
+document.write(`
+    <h1><img src="img/28.gif" alt="AdvancedMaceMastery" style="width:40px;height:40px;display:inline;">&nbsp; Advanced Mace Mastery</h1>
+    <fieldset><legend>[Passive]</legend>
+        <p> 
+            Increases you mastery with blunt weapons and gains <b>2/4/6/8/10/12/14/16/18/20</b> % weapon ATK.
+        </p>
+        <small>
+            Mace-type weapon is required.
+        </small>
+    </fieldset>
+`);
