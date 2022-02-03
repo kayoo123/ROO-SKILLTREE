d924abffac00d161b20fb19dab5244515c6330e2
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 15:01:21 +0100

    add

---
+++ hp/skill/24-Meditatio.js
@@ -0,0 +1,8 @@
+document.write(`
+    <h1><img src="img/24.gif" alt="Meditatio" style="width:40px;height:40px;display:inline;">&nbsp; Meditatio</h1>
+    <fieldset><legend>[Passive]</legend>	
+        <p> 
+            Permanently increases your max SP by <b>1/2/3/4/5/6/7/8/9/10</b> %, SP Regen by <b>3/6/9/12/15/18/21/24/27/30</b> % and the healing amount of Heal by <b>2/4/6/8/10/12/14/16/18/20</b> %.
+        </p>
+    </fieldset>
+`);
