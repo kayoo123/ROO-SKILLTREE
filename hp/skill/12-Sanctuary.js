30e732221cb7f720e2546486cc81670d82b3e868
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 11:51:02 +0100

    add

---
+++ hp/skill/12-Sanctuary.js
@@ -0,0 +1,35 @@
+document.write(`
+    <h1><img src="img/12.gif" alt="Sanctuary" style="width:40px;height:40px;display:inline;">&nbsp; Sanctuary</h1>
+    <fieldset><legend>[Support] [Holy]</legend>
+        <p> 
+            Summon the light of holy spirits, retoring <b>100/200/300/400/500/600/700/800/900/1000</b> HP per second to allied targets within range. <br />
+            When specific types of monster enter the range, they take <b>50/100/150/200/250/300/350/400/450/500</b> Holy Magic damage and will be knocked back. <br />
+            Sanctuary can only affect <b>4/5/6/7/8/9/10/11/12/13</b> targets, and it lasts <b>3/4/5/6/7/8/9/10/11/12</b> seconds.
+        </p>
+        <small>
+            Applies to Undead and Demon monsters.<br />
+            Effective targets include allied targets and monsters.
+        </small>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>(0.80s + XXs)</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>15/18/21/24/27/30/33/36/39/42</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
