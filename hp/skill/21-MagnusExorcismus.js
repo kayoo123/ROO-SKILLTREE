59f78e933dc1441c7f8aecd843f60d6d38aa9a4d
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 14:50:26 +0100

    add

--- hp/skill/21-MagnusExorcismus.js
+++ hp/skill/21-MagnusExorcismus.js
@@ -2,7 +2,8 @@ document.write(`
     <h1><img src="img/21.gif" alt="MagnusExorcismus" style="width:40px;height:40px;display:inline;">&nbsp; Magnus Exorcismus</h1>
     <fieldset><legend>[Magic Spell] [Holy]</legend>
         <p> 
-            Grants Holy attribute to a team member's weapon for <b>60/300</b> seconds
+            Summon a Cross Magic Circle at the target are, dealing <b>67/167/200/311/444/450/583/733/750/867</b> % Holy Magic Damage to all monsters. <br />
+            The magic circle lasts <b>5/6/7/8/9/10/11/12/13/14</b> seconds.
         </p>
         <small>
             Holy Attributes deal more damage to Undead and Shadow monsters.
@@ -12,15 +13,15 @@ document.write(`
             <table>
                 <tr>
                     <td>Cast Time:</td>
-                    <td>Neutral</td>
+                    <td>(2.50s + XXs)</td>
                 </tr>
                 <tr>
                     <td>Cooldown:</td>
-                    <td>Neutral</td>
+                    <td>5.0s</td>
                 </tr>
                 <tr>
                     <td>SP Cost:</td>
-                    <td>10/20</td>
+                    <td>80/85/90/95/100/105/110/115/120/125</td>
                 </tr>
                 <tr>
                     <td>Global Recovery:</td>
@@ -28,7 +29,7 @@ document.write(`
                 </tr>
                 <tr>
                     <td>Special Cost:</td>
-                    <td>[Holy Water]*1</td>
+                    <td>[Blue Gemstone]*1</td>
                 </tr>
             </table>
         </p>
