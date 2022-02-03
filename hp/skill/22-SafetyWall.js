5ea0b6740294c39beb12d1c43e3f580e3b5de49d
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 14:55:54 +0100

    dd

---
+++ hp/skill/22-SafetyWall.js
@@ -0,0 +1,35 @@
+document.write(`
+    <h1><img src="img/22.gif" alt="SafetyWall" style="width:40px;height:40px;display:inline;">&nbsp; Safety Wall</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Creates a defensives barrier within the target range. <br />
+            The first ally, whether it's one of your teammates or it's yourself, that enters that range gains a buff that prevents them from all Melee Physical damage by 100%. <br />
+            Safety Wall lasts <b>5/10/15/20/25/30/35/40/45/50</b> seconds, and it can tak up to <b>1/2/3/4/5/6/7/8/9/10</b> hits.
+        </p>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>(0.80/0.72/0.64/0.56/0.48/0.40/0.32/0.24/0.16/0.08 s + XXs)</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>50</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.0s</td>
+                </tr>
+                <tr>
+                    <td>Special Cost:</td>
+                    <td>[Blue Gemstone]*1</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
