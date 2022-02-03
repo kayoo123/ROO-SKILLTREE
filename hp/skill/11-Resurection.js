5e56186a9789ccf782d358ff9040156efc8c938b
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 11:39:08 +0100

    add

---
+++ hp/skill/11-Resurection.js
@@ -0,0 +1,33 @@
+document.write(`
+    <h1><img src="img/11.gif" alt="Resurection" style="width:40px;height:40px;display:inline;">&nbsp; Resurection</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Utilizes the power of angel to resurrect an ally target that has 0 HP and heals them back to <b>10/30/50/80</b> %*Max HP.
+        </p>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>(0.00s + XXs)</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>4.5s</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>50</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>Special Cost:</td>
+                    <td>[Blue Gemstone]*1</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
