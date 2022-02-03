556df09a48a31d0bb105655b1ae88144ed08e34b
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 14:58:28 +0100

    add

---
+++ hp/skill/23-Gloria.js
@@ -0,0 +1,29 @@
+document.write(`
+    <h1><img src="img/23.gif" alt="Gloria" style="width:40px;height:40px;display:inline;">&nbsp; Gloria</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Grants all your team members Glora status, increasing their LUK by 30 for <b>30/36/42/48/60</b> seconds
+        </p>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>30</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>2.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
