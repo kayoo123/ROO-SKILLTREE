7bcd9f49b8b0ecf56075caa848cf1b8906bb6b40
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 14:02:58 +0100

    add

---
+++ hp/skill/17-Pneuma.js
@@ -0,0 +1,29 @@
+document.write(`
+    <h1><img src="img/17.gif" alt="Pneuma" style="width:40px;height:40px;display:inline;">&nbsp; Pneuma</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Pneuma lasts <b>5/10/15/20/25/30/35/40/45/50</b> seconds, and it can take up to <b>1/2/3/4/5/6/7/8/9/10</b> hits while it's active.
+        </p>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>(0.80/0.72/0.64/0.56/0.48/0.40/0.32/0.24/0.16/0.08 s + XXs)</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>50</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
