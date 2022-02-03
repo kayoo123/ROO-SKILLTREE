7050a04468545ceffe1d8e71966343cb515e1866
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 15:37:04 +0100

    add

---
+++ hp/skill/33-HolyWard.js
@@ -0,0 +1,8 @@
+document.write(`
+    <h1><img src="img/33.gif" alt="HolyWard" style="width:40px;height:40px;display:inline;">&nbsp; Holy Ward</h1>
+    <fieldset><legend>[Passive]</legend>
+        <p> 
+            God's messenger that bathes in the Holy Light increases your Holy damage by <b>2/4/6/8/10/12/14/16/18/20</b> %.
+        </p>
+    </fieldset>
+`);
