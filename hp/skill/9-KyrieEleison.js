320f5424fc534d39795a3475dcd238d4b7ec26fd
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 11:30:38 +0100

    add

---
+++ hp/skill/9-KyrieEleison.js
@@ -0,0 +1,34 @@
+document.write(`
+    <h1><img src="img/9.gif" alt="KyrieEleison" style="width:40px;height:40px;display:inline;">&nbsp; Kyrie Eleison</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Designates an allied target and creates a protective shield for them. <br />
+            While it's acive, it can prevent physical damage that equals <b>10/14/18/22/26/30/34/38/42/46</b>% of their max HP. <br />
+            The shield can stop the damage for <b>5/6/6/7/7/8/8/9/9/10</b> times at max, and it lasts 10 seconds.
+        </p>
+        <small>
+            The shield immediatly loses its effect once being attacked by [Holy Light].
+        </small>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>5.0s</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>21/23/25/27/29/31/33/35/37/39</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
