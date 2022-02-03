b5a7755395c52f9edabd2574144e3d5c2660e5c4
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 15:17:25 +0100

    Add

---
+++ hp/skill/27-Judex.js
@@ -0,0 +1,29 @@
+document.write(`
+    <h1><img src="img/27.gif" alt="Judex" style="width:40px;height:40px;display:inline;">&nbsp; Judex</h1>
+    <fieldset><legend>[Magic Spell] [Holy]</legend>
+        <p> 
+            Prays for the the Holy Light to cleanse your enemy, dealing <b>340/380/420/460/500/540/580/620/660/700</b> Holy Magic dmage to your choosen target and up to 10 nearby monsters.
+        </p>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>(0.0s + XXs)</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>32/34/36/38/40/42/44/46/48/50</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>0.5s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
