365be60ac18fb835cd0f65eec8eee39d1c3d4f41
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 15:09:51 +0100

    add

---
+++ hp/skill/25-Assumptio.js
@@ -0,0 +1,33 @@
+document.write(`
+    <h1><img src="img/25.gif" alt="Assumptio" style="width:40px;height:40px;display:inline;">&nbsp; Assumptio</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Blesses every teammate with Assumptio, increasing their gears'DEF and MDEF by <b>55/60/65/70/75/80/85/90/95/100</b> % for <b>35/45/55/65/75/85/90/95/100</b> seconds.
+        </p>
+        <small>
+            This cannot coexist with Deafening Wardrum. <br />
+            Assumptio and Kyrie Eleison cannot coexist.
+        </small>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>(0.6s + XXs)</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>20/30/40/50/60/70/80/90/100/110</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.1/1.2/1.3/1.4/1.5/1.6/1.7/1.8/1.9/2 s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
