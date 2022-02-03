303802de13419af5bbc0ad5440e907ef0ab65757
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 12:08:41 +0100

    add

---
+++ hp/skill/14-Recovery.js
@@ -0,0 +1,36 @@
+document.write(`
+    <h1><img src="img/14.gif" alt="Recovery" style="width:40px;height:40px;display:inline;">&nbsp; Recovery</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Removes all Status Effects of your Partymates. <br/ >
+            As your skill level progresses, types of effect that you're able to remove expand. <br />
+            You will be still able to remove Abnormal Statuses you could remove before your skill levelled up.
+        </p>
+        <small>
+            lv.1 Removes Blind & Silence Status Effect. <br />
+            lv.2 Includes in the ability to remove Curse Status Effect. <br />
+            lv.3 Includes in the ability to remove Frozen, Stone Curse and Paralyze Status Effect.
+        </small>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>7.0s</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>10/15/20</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
