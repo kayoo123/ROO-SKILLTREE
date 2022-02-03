8afdc290d4b5d28d9788243a107f2dce29c66846
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 12:19:14 +0100

    add

---
+++ hp/skill/15-ImpositioManus.js
@@ -0,0 +1,30 @@
+document.write(`
+    <h1><img src="img/15.gif" alt="ImpositioManus" style="width:40px;height:40px;display:inline;">&nbsp; Impositio Manus</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Grants all your team members Impositio Manus status, increasing their ATK by <b>5/10/15/20/25/30/35/40/45/50</b>. <br />
+            At skill lv.10, additionnally increases your ATK by 50 and the Physical Damage done of your gears by 20% for 300 seconds.
+        </p>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>13/16/19/22/25/28/31/34/37/40</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
