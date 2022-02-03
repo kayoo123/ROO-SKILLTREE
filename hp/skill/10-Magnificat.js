59f1eab617a75c42e0fe1130298cf69ae2d5a7bc
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 11:34:02 +0100

    add

---
+++ hp/skill/10-Magnificat.js
@@ -0,0 +1,29 @@
+document.write(`
+    <h1><img src="img/10.gif" alt="Magnificat" style="width:40px;height:40px;display:inline;">&nbsp; Magnificat</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Grants all your team members Magnificat status, doubling their Magic Recovery Speed for <b>30/45/60/75/90</b> seconds.
+        </p>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>(0.80s + XXs)</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>40</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
