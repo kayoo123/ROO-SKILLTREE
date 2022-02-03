b919ab6d0e641f3ebfaed7c144f05e97f9d14175
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 14:17:27 +0100

    add

---
+++ hp/skill/18-HolyJudgment.js
@@ -0,0 +1,37 @@
+document.write(`
+    <h1><img src="img/18.gif" alt="HolyJudgment" style="width:40px;height:40px;display:inline;">&nbsp; Holy Judgment</h1>
+    <fieldset><legend>[Physical] [Neutral]</legend>
+        <p> 
+            Active Effect: <br />
+            Attacks a single target, and there's a <br />
+            <ul>
+		        <li>75% chance to deal <b>210/220/230/240/250/260/270/280/290/300</b> % neutral physical damage,</li>
+                <li>15% chance to deal <b>110/120/130/140/150/160/170/180/190/200</b> % neutral physical damage, and</li>
+                <li>10% chance to deal <b>320/340/360/380/400/420/440/460/480/500</b> % neutral physical damage to them</li>            
+            </ul>
+            Passive Effect: <br />
+            There's a <b>5/7/9/11/13/15/17/19/20</b> % chance to deal <b>110/120/130/140/150/160/170/180/190/200</b> % neutral physical damage with Basic Attacks.
+        </p>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>1.0s</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>12</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
