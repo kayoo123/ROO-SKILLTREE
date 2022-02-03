ceb2e3ca01bd845cf04f73be955aa649119ef1a4
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 15:27:04 +0100

    Add

---
+++ hp/skill/30-Suffragium.js
@@ -0,0 +1,31 @@
+document.write(`
+    <h1><img src="img/30.gif" alt="Suffragium" style="width:40px;height:40px;display:inline;">&nbsp; Suffragium</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Prays for blessings for you and one of your Partymates. <br />
+            Reducing their cast time required for the next skill by <b>15/30/45</b> % for <b>30/20/10</b> seconds. <br />
+            The blessing effect lasts until the skill duration is over or until casting a skill for 1 time.
+        </p>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>10/34/36/38/40/42/44/46/48/50</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>2.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
