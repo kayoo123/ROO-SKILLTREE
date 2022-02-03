5296db29c712aac74d697d8400baa52b5b7d7a7e
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 14:32:43 +0100

    add

---
+++ hp/skill/20-ZenHeart.js
@@ -0,0 +1,9 @@
+document.write(`
+    <h1><img src="img/22.gif" alt="ZenHeart" style="width:40px;height:40px;display:inline;">&nbsp; Zen Heart</h1>
+    <fieldset><legend>[Passive]</legend>	
+        <p> 
+            Permanently increases your natural SP Recovery Speed to (<b>3/5/9/12/15/18/21/24/27/30</b> + <b>0.2/0.4/0.6/0.8/1/1.2/1.4/1.6/1.8/2</b> %*Max SP) every 10 seconds. <br />
+            When using SP Recovery items, increases their restoring effects to <b>105/110/115/120/125/130/135/140/145/150</b> %.
+        </p>
+    </fieldset>
+`);
