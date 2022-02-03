d34ff003fe85d0f04f82ead405f1d5f37a89fb0d
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 15:22:13 +0100

    add

---
+++ hp/skill/29-ManaRecharge.js
@@ -0,0 +1,8 @@
+document.write(`
+    <h1><img src="img/29.gif" alt="ManaRecharge" style="width:40px;height:40px;display:inline;">&nbsp; Mana Recharge</h1>
+    <fieldset><legend>[Passive]</legend>
+        <p> 
+            Permanently decreases skill SP cost by <b>4/8/12/16/20</b> % .
+        </p>
+    </fieldset>
+`);
