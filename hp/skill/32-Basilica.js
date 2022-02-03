47143b9597ea40a954ed95e85b1ec0a7edc85b35
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 15:35:02 +0100

    add

---
+++ hp/skill/32-Basilica.js
@@ -0,0 +1,36 @@
+document.write(`
+    <h1><img src="img/32.gif" alt="Basilica" style="width:40px;height:40px;display:inline;">&nbsp; Basilica</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Creates a Holy Barrier at the target area, allowing targets within to become immune to damage. <br />
+            After the barrier is gone, heals them for <b>3/6/9/12/15</b> % of their HP. <br />
+            This effect lasts <b>5/7/9/11/13</b> seconds.
+        </p>
+        <small>
+            While the skill is active, the caster cannot engage in any other activity. <br />
+            You can't cast skills when you're inside the barrier. <br />
+            You can't use items when you're inside the barrier.
+        </small>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>(2.0s + XXs)</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>70s</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>80/90/100/110/120</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
