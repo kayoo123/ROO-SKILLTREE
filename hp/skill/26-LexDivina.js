102c21ab57ff239b8607cf85f70defbf65db6d11
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 15:13:24 +0100

    add

---
+++ hp/skill/26-LexDivina.js
@@ -0,0 +1,34 @@
+document.write(`
+    <h1><img src="img/26.gif" alt="LexDivina" style="width:40px;height:40px;display:inline;">&nbsp; Lex Divina</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Can be cast on a single target. <br />.
+            It has a chance to Silence the target and make them lose the ability to use skills. <br />
+            This effect lasts <b>10/15/20/25/30</b> seconds.
+        </p>
+        <small>
+            Silence chance is subject to the target's VIT attribute.
+        </small>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>50/45/40/35/30</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>3.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
