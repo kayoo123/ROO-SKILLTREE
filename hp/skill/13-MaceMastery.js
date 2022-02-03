c67ca999a95bbc37fb3b72055b37db0bead59b4d
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 12:02:19 +0100

    add

---
+++ hp/skill/13-MaceMastery.js
@@ -0,0 +1,11 @@
+document.write(`
+    <h1><img src="img/13.gif" alt="MaceMastery" style="width:40px;height:40px;display:inline;">&nbsp; Mace Mastery</h1>
+    <fieldset><legend>[Passive]</legend>
+        <p> 
+            Increases Mace Weapon Mastery and increases your Physical attack by <b>3/6/9/12/15/18/21/24/27/30</b>.
+        </p>
+        <small>
+            Mace-type weapon is required.
+        </small>
+    </fieldset>
+`);
