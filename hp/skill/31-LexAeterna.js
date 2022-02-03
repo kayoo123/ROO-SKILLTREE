8ed0a087e11abb4f81ba05dc833418531869a354
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 15:28:52 +0100

    dd

---
+++ hp/skill/31-LexAeterna.js
@@ -0,0 +1,29 @@
+document.write(`
+    <h1><img src="img/31.gif" alt="LexAeterna" style="width:40px;height:40px;display:inline;">&nbsp; Lex Aeterna</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            By the power of angels, your target takes <b>20/40/60/80/100</b> % more damage the next hit.
+        </p>
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>30</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>3.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
