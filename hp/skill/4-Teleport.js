aaf221ce977b5949605bc0ab468304cd3657930f
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 10:25:54 +0100

    test

---
+++ hp/skill/4-Teleport.js
@@ -0,0 +1,33 @@
+document.write(`
+    <h1><img src="img/4.gif" alt="Teleport" style="width:50px;height:50px;display:inline;">&nbsp; Teleport</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Teleports to any location on the map. <br />
+            It has a similar effect to the item "Fly Wings".
+        </p>
+        <small>
+            This skill cannot be used for auto-battle.
+        </small>	
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>1.0s</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>40</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
---
+++ hp/skill/5-Blessing.js
@@ -0,0 +1,30 @@
+document.write(`
+    <h1><img src="img/5.gif" alt="Blessing" style="width:50px;height:50px;display:inline;">&nbsp; Blessing</h1>
+    <fieldset><legend>[Support] [Neutral]</legend>
+        <p> 
+            Increase certain attributes of your target and teammates, including :<br />
+            +<b>1/2/3/4/5/6/7/8/9/10</b> STR, DEX and INT for <b>240/280/320/360/400/440/480/520/560/600</b> seconds.
+        </p>	
+        <hr>
+        <p> 
+            <table>
+                <tr>
+                    <td>Cast Time:</td>
+                    <td>Neutral</td>
+                </tr>
+                <tr>
+                    <td>Cooldown:</td>
+                    <td>1.0s</td>
+                </tr>
+                <tr>
+                    <td>SP Cost:</td>
+                    <td>20/24/28/32/36/40/44/48/52/56</td>
+                </tr>
+                <tr>
+                    <td>Global Recovery:</td>
+                    <td>1.0s</td>
+                </tr>
+            </table>
+        </p>
+    </fieldset>
+`);
