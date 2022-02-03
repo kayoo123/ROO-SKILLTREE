496477ead39ad32bab56e6755ec0443b03879045
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-03 11:23:18 +0100

    add

--- hp/skill/8-IncreaseAgility.js
+++ hp/skill/8-IncreaseAgility.js
@@ -2,12 +2,11 @@ document.write(`
     <h1><img src="img/8.gif" alt="IncreaseAgility" style="width:40px;height:40px;display:inline;">&nbsp; Increase Agility</h1>
     <fieldset><legend>[Passive]</legend>	
         <p> 
-            Gains <b>2/4/6/8/10/12/14/16/18/20</b> Base Physical Attack and <b>2/4/6/8/10/12/14/16/18/20</b> Base Physical Defense. <br />
-            While attacking Undead or Demon monsters, gains <b>5/10/15/20/25/30/35/50/45/50</b> extra Base Physical Attack. <br />
-            When attacked by Undead or Demon monsters, gains <b>5/10/15/20/25/30/35/50/45/50</b> extra Base Physical Defense.
+            Grants Blessings status additional buffs, including: <br />
+            AGI +<b>4/6/8/10/12</b> and a 25% MSPD bonus.
         </p>
         <small>
-            Attributes additionnaly gained only apply when you're battling Undead or Demon monsters
+            The active duration of Increase Agility and Blessing is the same.
         </small>
     </fieldset>
 `);
