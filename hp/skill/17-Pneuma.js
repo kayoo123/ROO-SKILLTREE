bcc9a57438454d9bb201bf57b9b352a4bc697669
Jeremi Desesquelles <jeremi.desesquelles@orange.com>
2022-02-07 17:12:36 +0100

    fix

--- hp/skill/17-Pneuma.js
+++ hp/skill/17-Pneuma.js
@@ -2,6 +2,8 @@ document.write(`
     <h1><img src="img/17.gif" alt="Pneuma" style="width:40px;height:40px;display:inline;">&nbsp; Pneuma</h1>
     <fieldset><legend>[Support] [Neutral]</legend>
         <p> 
+            Creates a defensives barrier within the target range. <br />
+            The first ally, whether it's one of your teammates or it's yourself, that enters that range gains a buff that prevents them from all Ranged Physical damage by 100%. <br />
             Pneuma lasts <b>5/10/15/20/25/30/35/40/45/50</b> seconds, and it can take up to <b>1/2/3/4/5/6/7/8/9/10</b> hits while it's active.
         </p>
         <hr>
